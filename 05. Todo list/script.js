3cf250e754a4745d8caf5d8743ca87e879600d3c
bitwizcoder <mhmdnoman01@gmail.com>
2024-03-17 01:32:14 +0600

    added local storage functionalities

--- 05. Todo list/script.js
+++ 05. Todo list/script.js
@@ -1,1 +1,39 @@
-console.log("Hello from script.");
+// Elements
+const addBtn = document.querySelector(".add-btn");
+const taskInput = document.querySelector(".task-input");
+const taskUl = document.querySelector(".task-ul");
+const taskLi = document.querySelector(".task-li");
+
+// Store todos in local storage
+// Get todos from local storage
+// Show message for adding tasks if no task are stored
+// Delete Tasks
+// Add Edit Button
+
+// https://gomakethings.com/updating-your-ui-based-on-user-inputs-with-vanilla-javascript/
+
+// Store todos in local storage
+const taskArray = [];
+
+addBtn.addEventListener("click", () => {
+  if (taskInput.value.length < 1) return;
+
+  taskArray.push(taskInput.value);
+
+  taskUl.innerHTML += "<li>" + taskInput.value + "</li>";
+
+  localStorage.setItem("tasks", JSON.stringify(taskArray));
+
+  taskInput.value = "";
+});
+
+// Get todos from local storage
+const taskData = JSON.parse(localStorage.getItem("tasks"));
+taskData.map((item) => {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+  taskUl.appendChild(li);
+  console.log(item);
+});
+
+// Delete Tasks
